164917594a012dab6a021ca4aaa3bd25ffdb6a24
mskwon1 <mageeeeek@gmail.com>
2020-03-18 19:13:37 +0900

    fix: change birthday field type and delete sqlite file

--- server/apps/api/models.py
+++ server/apps/api/models.py
@@ -11,7 +11,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
-    birthday = models.DateTimeField(null=True)
+    birthday = models.DateField(null=True)
     
     GENDER_CHOICES = [
         (True, 'Man'),
