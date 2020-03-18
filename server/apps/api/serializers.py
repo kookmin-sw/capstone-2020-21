27d1b9169ea86d49c9c619e2f57c9830b8d57021
mskwon1 <mageeeeek@gmail.com>
2020-03-18 17:57:13 +0900

    feat: make user serializer return human-readable value for gender

--- server/apps/api/serializers.py
+++ server/apps/api/serializers.py
@@ -2,6 +2,8 @@ from rest_framework import serializers
 from .models import User
 
 class UserSerializer(serializers.ModelSerializer):
+    gender = serializers.CharField(source='get_gender_display')
+    
     class Meta:
         model = User
         fields = ['id', 'username', 'gender', 'birthday']
