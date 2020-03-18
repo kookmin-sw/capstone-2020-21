077be167ccce4df39a1d9ee20c8cbbbba6f3a70d
gyqls980 <hyobin0715@kookmin.ac.kr>
2020-03-18 21:46:03 +0900

    feat: add Clothes, ClothesSet, ClothesSetReview model

--- server/apps/api/models.py
+++ server/apps/api/models.py
@@ -26,7 +26,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     
     def __str__(self):
         return self.username
-# Create your models here.
+
 
 class Clothes(models.Model):
 
@@ -41,16 +41,50 @@ class Clothes(models.Model):
     LOWER_CATEGORY_CHOICES = [
         (1, 'Jeans'),
         (2, 'Knitwear'),
-        (3, 'Jacket')
+        (3, 'Jacket'),
     ]
 
-    upper_category = models.CharField(max_length=10, choices=UPPER_CATEGORY_CHOICES)
-    lower_category = models.CharField(max_length=20, choices=LOWER_CATEGORY_CHOICES)
+    upper_category = models.CharField(max_length=30, choices=UPPER_CATEGORY_CHOICES)
+    lower_category = models.CharField(max_length=30, choices=LOWER_CATEGORY_CHOICES)
     image_url = models.URLField(unique=True)
-    alias = models.CharField(max_length=20, null=True)
-    # owner = models.ForeignKey('Users', on_delete=models.CASCADE)
+    alias = models.CharField(max_length=30, null=True)
+    owner = models.ForeignKey('User', on_delete=models.CASCADE)
 
 
-# class ClothesSet(models.Model):
+class ClothesSet(models.Model):
+
+    chothes = models.ManyToManyField(Clothes)
+    name = models.CharField(max_length=30)
+    style = models.CharField(max_length=30)
+    image_url = models.URLField(unique=True)
+    owner = models.ForeignKey('User', on_delete=models.CASCADE)
 
 
+class ClothesSetReview(models.Model):
+
+    LOCATION_CHOICES = [
+        (1, '서울특별시'),
+        (2, '천안시'),
+        (3, '부산광역시'),
+    ]
+
+    REVIEW_CHOICES = [
+        (1, 'Warm'),
+        (2, 'Cool'),
+        (3, 'Hot'),
+        (4, 'Cold'),
+    ]
+
+    clothes_set = models.ForeignKey('ClothesSet', on_delete=models.CASCADE)
+    start_datetime = models.DateTimeField()
+    end_datetime = models.DateTimeField()
+    location = models.CharField(max_length=50, choices=LOCATION_CHOICES)
+    review = models.CharField(max_length=30, choices=REVIEW_CHOICES)
+    max_temp = models.FloatField()
+    min_temp = models.FloatField()
+    max_sensible_temp = models.FloatField()
+    min_sensible_temp = models.FloatField()
+    humidity = models.IntegerField()
+    wind_speed = models.FloatField()
+    precipitation = models.IntegerField()
+    owner = models.ForeignKey('User', on_delete=models.CASCADE)
