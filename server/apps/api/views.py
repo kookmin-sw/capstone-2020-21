fe52e9c92aac86a52c5dd57dcf501a2e2b2d7d9a
mskwon1 <mageeeeek@gmail.com>
2020-03-18 17:55:00 +0900

    feat: add simple endpoints for user model

--- server/apps/api/views.py
+++ server/apps/api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import User
+from .serializers import UserSerializer
 
-# Create your views here.
+class UserView(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    def get_queryset(self):
+        # # This code is to test if request.user is received correctly
+        # if self.request.user.is_authenticated:
+        #     print(self.request.user)
+        return super().get_queryset()
+    
