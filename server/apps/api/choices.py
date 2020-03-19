5c1f9af1c6e5a503884361b0a5ecc1052ac15b77
gyqls980 <hyobin0715@kookmin.ac.kr>
2020-03-19 20:56:21 +0900

    feat: add lower_category_choices

--- server/apps/api/choices.py
+++ server/apps/api/choices.py
@@ -2,35 +2,61 @@
 # 알파벳 순으로 정렬.
 
 GENDER_CHOICES = [
-    (True, 'Man'),
-    (False, 'Woman'),
+    (True, 'man'),
+    (False, 'woman'),
 ]
 
 # TODO : 수정필요
 LOCATION_CHOICES = [
-    (1, '서울특별시'),
-    (2, '천안시'),
-    (3, '부산광역시'),
+    ('cheonan', 'cheonan'),
+    ('busan', 'busan'),
+    ('seoul', 'seoul'),
 ]
 
-# TODO : 수정필요
 LOWER_CATEGORY_CHOICES = [
-    (1, 'Jeans'),
-    (2, 'Knitwear'),
-    (3, 'Jacket'),
+    ('blazer', 'blazer'),
+    ('blouse', 'blouse'),
+    ('blue_jacket', 'blue_jacket'),
+    ('coach_jacket', 'coach_jacket'),
+    ('coat', 'coat'),
+    ('field_jacket', 'field_jacket'),
+    ('golden_pants', 'golden_pants'),
+    ('hoodie', 'hoodie'),
+    ('hot_pants', 'hot_pants'),
+    ('jeans', 'jeans'),
+    ('leather_jacket', 'leather_jacket'),
+    ('long_padding', 'long_padding'),
+    ('long_skirt', 'long_skirt'),
+    ('long_sleeve', 'long_sleeve'),
+    ('long_sleeve_shirt', 'long_sleeve_shirt'),
+    ('mustang', 'mustang'),
+    ('short_padding', 'short_padding'),
+    ('short_sleeve', 'short_sleeve'),
+    ('short_sleeve_shirt', 'short_sleeve_shirt'),
+    ('shorts', 'shorts'),
+    ('skirt', 'skirt'),
+    ('slacks', 'slacks'),
+    ('stadium_jacket', 'stadium_jacket'),
+    ('sweater', 'sweater'),
+    ('sweatpants', 'sweatpants'),
+    ('sweatshirt', 'sweatshirt'),
+    ('track_top', 'track_top'),
+    ('turtleneck', 'turtleneck'),
+    ('vest_padding', 'vest_padding'),
+    ('windbreaker', 'windbreaker'),
 ]
 
 REVIEW_CHOICES = [
-    (1, 'Warm'),
-    (2, 'Cool'),
-    (3, 'Hot'),
-    (4, 'Cold'),
+    ('cold', 'cold'),
+    ('cool', 'cool'),
+    ('hot', 'hot'),
+    ('warm', 'warm'),
 ]
     
 UPPER_CATEGORY_CHOICES = [
-    (1, 'Top'),
-    (2, 'Bottom'),
-    (3, 'Outer'),
-    (4, 'Dress'),
-    (5, 'Skirt'),
+    ('bottom', 'bottom'),
+    ('dress', 'dress'),
+    ('outer', 'outer'),
+    ('skirt', 'skirt'),
+    ('top', 'top'),
 ]
