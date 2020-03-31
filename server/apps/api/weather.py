07181ab3739281bf270c90f56185d686a752fa49
JaehoLee <hiljh96@naver.com>
2020-03-31 20:00:16 +0900

    Edit annotation and JSON DATA LOAD

--- server/apps/api/weather.py
+++ server/apps/api/weather.py
@@ -11,14 +11,15 @@ from pprint import pprint
 ServiceKey = settings.WEATHER_API_KEY_1
 # ServiceKey = settings.SERVICE_API_KEY
 
-def get_weather_date(input_date, location) : # 기본
+# 날씨와 장소를 인자로 받아서 날씨 데이터 딕셔너리를 반환한다.
+# 예시 input_date : 2020-03-31 15:26:23, location : "1" location index
+def get_weather_date(input_date, location) : 
 
-    with open('apps/api/locations/data_full_address.json') as json_file:
+    with open('apps/api/locations/data.json') as json_file:
         json_data = json.load(json_file)
 
     days = input_date.split()
     times= days[0].split('-')
-
     month = times[1]
     day = times[2]
 
@@ -26,14 +27,8 @@ def get_weather_date(input_date, location) : # 기본
     times1 = days[1].split(':')
     api_time =times1[0]+times1[1]
 
-    # print(month)
-    # print(day)
-
     convert_api_time, convert_api_date = convert_time(api_time, month, day)
-
     convert_api_date = times[0] + convert_api_date # times[0] -> 년도 년도 합쳐주기
-    # print( "convert_time : " + convert_api_time)
-    # print("convert_date : " + convert_api_date)
 
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?"
     key = "serviceKey=" + ServiceKey
@@ -49,45 +44,27 @@ def get_weather_date(input_date, location) : # 기본
     ny = "&ny=" + y
 
     api_url = url + key + numOfRows + type + date + time + nx + ny
-
-    # print(api_url)
-
     data = urllib.request.urlopen(api_url).read().decode('utf8')
     data_json = json.loads(data)
-    # pprint(data_json)
-
+    # get date and time
     parsed_json = data_json['response']['body']['items']['item']
-    target_date = parsed_json[0]['fcstDate']  # get date and time
+    target_date = parsed_json[0]['fcstDate']  
     target_time = parsed_json[0]['fcstTime']
 
-    # date_calibrate = target_date #date of TMX, TMN
-    # if target_time > '1300':
-    #     date_calibrate = str(int(target_date) + 1)
-
     passing_data = {}
     for parsed in parsed_json:
         passing_data[parsed['category']] = parsed['fcstValue']
 
-
-    # parsed_json = data_json['response']['body']['items']['item']
-
-    # target_date = parsed_json[0]['fcstDate']  # get date and time
-    # target_time = parsed_json[0]['fcstTime']
-
-    # date_calibrate = target_date #date of TMX, TMN
-    # # if target_time > '1300':
-    # #     date_calibrate = str(int(target_date) + 1)
-
     passing_data = {}
     for parsed in parsed_json:
         passing_data[parsed['category']] = parsed['fcstValue']
-    
     
     t = passing_data['T3H']
     v = passing_data['WSD']
     t = float(t)
     v = float(v)
     
+    # 체감 온도 계산
     wci = 13.12 + 0.6215*t -  11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
     wci = round(wci, 2)
     passing_data['WCI'] = wci
@@ -97,29 +74,25 @@ def get_weather_date(input_date, location) : # 기본
     t_high = float(t_high)
     t_min = float(t_min)
 
-    
-   
     wci_high = 13.12 + 0.6215*t_high -  11.37*math.pow(v, 0.16) + 0.3965*t_high*math.pow(v, 0.16)
     wci_high = round(wci_high, 2)
     wci_low = 13.12 + 0.6215*t_min -  11.37*math.pow(v, 0.16) + 0.3965*t_min*math.pow(v, 0.16)
     wci_low = round(wci_low, 2)
 
     passing_data['WCI'] = wci # current wind chill temp
-    passing_data['WCIMAX'] = wci_high #maximum chill temp
-    passing_data['WCIMIN'] = wci_low #minium chill temp
+    passing_data['WCIMAX'] = wci_high # maximum chill temp
+    passing_data['WCIMIN'] = wci_low # minium chill temp
 
     return passing_data
 
+# 날씨 불러오기 날짜, 시간, location(index)
 def get_weather_by_time_date(date, time, location) :
 
-    with open('apps/api/locations/data_full_address.json') as json_file:
+    with open('apps/api/locations/data.json') as json_file:
         json_data = json.load(json_file)
 
     api_date = date
     api_time = time
-
-    # print(api_date)
-    # print(api_time)
 
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?"
     key = "serviceKey=" + ServiceKey
@@ -137,23 +110,18 @@ def get_weather_by_time_date(date, time, location) :
     api_url = url + key + numOfRows + type + date + time + nx + ny
     data = urllib.request.urlopen(api_url).read().decode('utf8')
     data_json = json.loads(data)
-    pprint(data_json)
 
     parsed_json = data_json['response']['body']['items']['item']
-
     target_date = parsed_json[0]['fcstDate']  # get date and time
     target_time = parsed_json[0]['fcstTime']
-
     date_calibrate = target_date #date of TMX, TMN
-    # if target_time > '1300':
-    #     date_calibrate = str(int(target_date) + 1)
-
     passing_data = {}
+
     for parsed in parsed_json:
         passing_data[parsed['category']] = parsed['fcstValue']
     
     t = passing_data['T3H'] # current 3hour temp
-    v = passing_data['WSD'] # current 
+    v = passing_data['WSD'] # current wind speed
 
     t = float(t)
     v = float(v)
@@ -161,12 +129,12 @@ def get_weather_by_time_date(date, time, location) :
     t_high = passing_data['TMX']
     t_min = passing_data['TMN']
 
-    wci = 13.12 + 0.6215*t -  11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
+    wci = 13.12 + 0.6215*t - 11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
     wci = round(wci, 2)
 
-    wci_high = 13.12 + 0.6215*t_high -  11.37*math.pow(v, 0.16) + 0.3965*t_high*math.pow(v, 0.16)
+    wci_high = 13.12 + 0.6215*t_high - 11.37*math.pow(v, 0.16) + 0.3965*t_high*math.pow(v, 0.16)
     wci_high = round(wci, 2)
-    wci_low = 13.12 + 0.6215*t_min -  11.37*math.pow(v, 0.16) + 0.3965*t_min*math.pow(v, 0.16)
+    wci_low = 13.12 + 0.6215*t_min - 11.37*math.pow(v, 0.16) + 0.3965*t_min*math.pow(v, 0.16)
     wci_low = round(wci, 2)
 
     passing_data['WCI'] = wci # current wind chill temp
@@ -175,6 +143,7 @@ def get_weather_by_time_date(date, time, location) :
 
     return passing_data
 
+# start_date와 end_date 사이에 날씨 API 발표시각이 있으면 중간 값 떼오기
 def get_weather_between(start_date, end_date, location) :
 
     with open('apps/api/locations/data_full_address.json') as json_file:
@@ -182,7 +151,7 @@ def get_weather_between(start_date, end_date, location) :
         
     start_days = start_date.split()
     start_times= start_days[0].split('-')
-    #times = yyyymmzdd  yyyy -> 년 mm ->달 dd --> 일
+    # times = yyyymmzdd  yyyy -> 년 mm ->달 dd --> 일
     end_days = end_date.split()
     end_times = end_date.spilt()
 
@@ -201,8 +170,9 @@ def get_weather_between(start_date, end_date, location) :
     end_T3H = float(end_weather['T3H'])
 
     passing_data = {}
-    #api_time tiems1는 시간 xxyy xx -> 시간 yy -> 분
-    #TODO 사이 시간에 API 호출 시간이 있으면 그시간 온도도 다시 구해오기
+    # api_time tiems1는 시간 xxyy xx -> 시간 yy -> 분
+    # TODO(Jaeho) 사이 시간에 API 호출 시간이 있으면 호출 가능한 시간 온도도 다시 구해오기, 구현 완료 후 삭제
+    # start와 end time 사이에 날씨 API 갱신 시간이 있으면 그시간을 불러옴
     if  end_api_time - start_api_time > 3  : # 3시간 이상 6시간 미만 이면
         between_api_time = end_api_time - start_api_time
         between_data =  get_weather_by_time_date(start_api_date, first_api_time, location)
@@ -215,73 +185,58 @@ def get_weather_between(start_date, end_date, location) :
         
     return passing_data
 
-def convert_time(time, month, day) : #입력 받은 시간을 API 요청가능 시간으로 바꿔주기
-
-    if int(time) < 200 :
-        print("passed in < 200")
+# 입력 받은 시간을 API 요청가능 시간으로 바꿔주기
+# 날씨 API에서 확정적으로 호출 가능한 시간은 0200, 0500, 0800, 1100, 1400, 1700, 2000, 2300 (1일 8회 3시간 간격)
+def convert_time(time, month, day) : 
+    # 2시 이전일 때 전날 23:00 날씨를 받아옴
+    if int(time) < 200 : 
         day =int(day)
         day = day-1
         month = int(month)
         if day == 0 :
             if month == 5 or 7 or 10 or 12 :
                 day = 30
-
             elif month == 1 or 2 or 4 or 6 or 8 or 9 or 11 :
                 day = 31
-
             elif month == 3 :
                 day = 28
-            
-            month = month-1
 
+            month = month-1
             str_month = str(month)
 
             if(month/10 == 0) :
                 str_month = "0" + str_month
-
             if int(day) / 10 == 0 :
                 str_day = "0" +str(day)
 
         time = "2300"
     
-    elif int(time) < 500 :
-
+    elif int(time) < 500 : # 05:00 이전 일 때 02:00 날씨를 받아옴
         time = "0200"
 
-    elif int(time) < 800 :
-        print('passed in <800')
+    elif int(time) < 800 : # 08:00 이전 일 때 05:00 날씨를 받아옴
         time = "0500"
 
-    elif int(time) < 1100 :
-
+    elif int(time) < 1100 : # 11:00 이전 일 때 08:00 날씨를 받아옴
         time = "0800"
     
-    elif int(time) < 1400 :
-
+    elif int(time) < 1400 : # 14:00 이전 일 때 14:00 날씨를 받아옴
         time = "1100"
 
-    elif int(time) < 1700 :
-
+    elif int(time) < 1700 : # 17:00 이전 일 때 14:00 날씨를 받아옴
         time = "1400"
 
-    elif int(time) < 2000 :
-
+    elif int(time) < 2000 : # 20:00 이전 일 때 17:00 날씨를 받아옴
         time = "1700"
 
     elif int(time) < 2300 :
-
         time = " 2000"
 
     elif int(time) < 2400 :
-
         time = "2300"
 
     str_day = str(day) 
     str_month = str(month)
-    # print(str_day)
-    # print(str_month)
-
     date = str_month+str_day
-    # print("time : " +  time)
 
     return time, date
