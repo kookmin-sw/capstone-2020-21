e0129a8142b678fcf3f44a9c8f2b111717ee0fa3
JaehoLee <hiljh96@naver.com>
2020-03-31 19:19:40 +0900

    Change for the PULL REQUEST and Create API TIME CONVERTER

--- server/apps/api/views.py
+++ server/apps/api/views.py
@@ -20,13 +20,13 @@ from .serializers import (
     ClothesSetReviewReadSerializer,
     UserSerializer
 )
+from .utils import *
 from .validations import (
     user_query_schema, 
     clothes_query_schema, 
     clothes_set_query_schema, 
     clothes_set_review_query_schema
 )
-from .utils import *
 from .weather import get_weather_date
 
 class UserView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewSet):
@@ -169,9 +169,9 @@ class ClothesView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewSet):
             return Response({
                 'error' : 'you are not allowed to access this object'
             }, status=status.HTTP_401_UNAUTHORIZED)
-            
+   
         return super().destroy(request, *args, **kwargs)
-        
+ 
     @action(detail=False, methods=['post'])
     def inference(self, request, *args, **kwargs):
         """
@@ -326,7 +326,6 @@ class ClothesSetView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewSet):
             
         return super().destroy(request, *args, **kwargs)
         
-    
 class ClothesSetReviewView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewSet):    
     def get_queryset(self):
         queryset = ClothesSetReview.objects.all()
@@ -479,10 +478,8 @@ class ClothesSetReviewView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewS
         #API 요청하기
         start_weather = get_weather_date(start, location)
         end_weather = get_weather_date(start, location)
-        
 
         #온도 구하기
-
         start_min = start_weather['TMN']
         start_max = start_weather['TMX']
         start_sense_max = start_weather['WCIMAX']
@@ -501,7 +498,6 @@ class ClothesSetReviewView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewS
         minimum_sensible_temp =  min(start_sense_min, end_sense_min)
 
         #데이터 저장하기
-
         weatherData = ClothesSetReview()
         weatherData.max_temp = maximum_temp
         weatherData.min_temp = minimum_temp
@@ -511,10 +507,7 @@ class ClothesSetReviewView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewS
         weatherData.precipitation =  start_weather['R06']
         weatherData.wind_speed = start_weather['WSD']
         weatherData.save()
-        
-       
-
-
+    
         return Response({
                 'min_temp': weatherData.min_temp,
                 'max_temp': weatherData.max_temp,
--- server/apps/api/weather.py
+++ server/apps/api/weather.py
@@ -4,44 +4,43 @@ import datetime
 import pytz
 import math
 from datetime import datetime
+from django.conf import settings
 from urllib.request import urlopen
 from pprint import pprint
-from django.conf import settings
 
-
-
-
-
-
-
-# ServiceKey = settings.WEATHER_API_KEY_1
+ServiceKey = settings.WEATHER_API_KEY_1
+# ServiceKey = settings.SERVICE_API_KEY
 
 def get_weather_date(input_date, location) : # 기본
+
     with open('apps/api/locations/data_full_address.json') as json_file:
         json_data = json.load(json_file)
 
-    # ServiceKey = settings.SERVICE_API_KEY
-    ServiceKey = settings.WEATHER_API_KEY_1
-
-
     days = input_date.split()
     times= days[0].split('-')
+
+    month = times[1]
+    day = times[2]
 
     api_date = times[0]+times[1]+times[2]
     times1 = days[1].split(':')
     api_time =times1[0]+times1[1]
 
+    # print(month)
+    # print(day)
 
-    # print(api_date)
-    # print(api_time)
+    convert_api_time, convert_api_date = convert_time(api_time, month, day)
+
+    convert_api_date = times[0] + convert_api_date # times[0] -> 년도 년도 합쳐주기
+    # print( "convert_time : " + convert_api_time)
+    # print("convert_date : " + convert_api_date)
 
     url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?"
     key = "serviceKey=" + ServiceKey
     numOfRows = "&numOfRows=100"
     type = "&dataType=JSON"
-    date = "&base_date=" + api_date
-    time = "&base_time=" + api_time
-
+    date = "&base_date=" + convert_api_date
+    time = "&base_time=" + convert_api_time
 
     x = (json_data[location]['x'])
     y = (json_data[location]['y'])
@@ -55,15 +54,13 @@ def get_weather_date(input_date, location) : # 기본
 
     data = urllib.request.urlopen(api_url).read().decode('utf8')
     data_json = json.loads(data)
-    pprint(data_json)
+    # pprint(data_json)
 
-    
     parsed_json = data_json['response']['body']['items']['item']
-
     target_date = parsed_json[0]['fcstDate']  # get date and time
     target_time = parsed_json[0]['fcstTime']
 
-    date_calibrate = target_date #date of TMX, TMN
+    # date_calibrate = target_date #date of TMX, TMN
     # if target_time > '1300':
     #     date_calibrate = str(int(target_date) + 1)
 
@@ -103,17 +100,15 @@ def get_weather_date(input_date, location) : # 기본
     
    
     wci_high = 13.12 + 0.6215*t_high -  11.37*math.pow(v, 0.16) + 0.3965*t_high*math.pow(v, 0.16)
-    wci_high = round(wci, 2)
+    wci_high = round(wci_high, 2)
     wci_low = 13.12 + 0.6215*t_min -  11.37*math.pow(v, 0.16) + 0.3965*t_min*math.pow(v, 0.16)
-    wci_low = round(wci, 2)
+    wci_low = round(wci_low, 2)
 
     passing_data['WCI'] = wci # current wind chill temp
     passing_data['WCIMAX'] = wci_high #maximum chill temp
     passing_data['WCIMIN'] = wci_low #minium chill temp
 
-
     return passing_data
-
 
 def get_weather_by_time_date(date, time, location) :
 
@@ -122,7 +117,6 @@ def get_weather_by_time_date(date, time, location) :
 
     api_date = date
     api_time = time
-
 
     # print(api_date)
     # print(api_time)
@@ -133,7 +127,6 @@ def get_weather_by_time_date(date, time, location) :
     type = "&dataType=JSON"
     date = "&base_date=" + api_date
     time = "&base_time=" + api_time
-
 
     x = (json_data[location]['x'])
     y = (json_data[location]['y'])
@@ -159,7 +152,6 @@ def get_weather_by_time_date(date, time, location) :
     for parsed in parsed_json:
         passing_data[parsed['category']] = parsed['fcstValue']
     
-    
     t = passing_data['T3H'] # current 3hour temp
     v = passing_data['WSD'] # current 
 
@@ -169,11 +161,9 @@ def get_weather_by_time_date(date, time, location) :
     t_high = passing_data['TMX']
     t_min = passing_data['TMN']
 
-    
     wci = 13.12 + 0.6215*t -  11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
     wci = round(wci, 2)
 
-    
     wci_high = 13.12 + 0.6215*t_high -  11.37*math.pow(v, 0.16) + 0.3965*t_high*math.pow(v, 0.16)
     wci_high = round(wci, 2)
     wci_low = 13.12 + 0.6215*t_min -  11.37*math.pow(v, 0.16) + 0.3965*t_min*math.pow(v, 0.16)
@@ -183,20 +173,13 @@ def get_weather_by_time_date(date, time, location) :
     passing_data['WCIMAX'] = wci_high #maximum chill temp
     passing_data['WCIMIN'] = wci_low #minium chill temp
 
-
     return passing_data
-
-
 
 def get_weather_between(start_date, end_date, location) :
 
-    
     with open('apps/api/locations/data_full_address.json') as json_file:
         json_data = json.load(json_file)
-
-    # ServiceKey = settings.SERVICE_API_KEY
         
-
     start_days = start_date.split()
     start_times= start_days[0].split('-')
     #times = yyyymmzdd  yyyy -> 년 mm ->달 dd --> 일
@@ -219,98 +202,86 @@ def get_weather_between(start_date, end_date, location) :
 
     passing_data = {}
     #api_time tiems1는 시간 xxyy xx -> 시간 yy -> 분
-    
+    #TODO 사이 시간에 API 호출 시간이 있으면 그시간 온도도 다시 구해오기
     if  end_api_time - start_api_time > 3  : # 3시간 이상 6시간 미만 이면
-        
         between_api_time = end_api_time - start_api_time
-
         between_data =  get_weather_by_time_date(start_api_date, first_api_time, location)
-
         between_T3H = float(between_data['T3H'])
-
         #MIN -> MAX TEMP
         passing_data['MINT'] = min(start_T3H, end_T3H, between_T3H)
         #MAX -> MIN TEMP
         passing_data['MAXT'] = max(start_T3H, end_T3H, between_T3H)
-
         passing_data['VVV'] = start_weather['WSD']
         
-    #TODO
-        
-
     return passing_data
 
+def convert_time(time, month, day) : #입력 받은 시간을 API 요청가능 시간으로 바꿔주기
 
+    if int(time) < 200 :
+        print("passed in < 200")
+        day =int(day)
+        day = day-1
+        month = int(month)
+        if day == 0 :
+            if month == 5 or 7 or 10 or 12 :
+                day = 30
 
+            elif month == 1 or 2 or 4 or 6 or 8 or 9 or 11 :
+                day = 31
 
+            elif month == 3 :
+                day = 28
+            
+            month = month-1
 
+            str_month = str(month)
 
+            if(month/10 == 0) :
+                str_month = "0" + str_month
 
+            if int(day) / 10 == 0 :
+                str_day = "0" +str(day)
 
-# --------------------- test code -------------------------
+        time = "2300"
+    
+    elif int(time) < 500 :
 
+        time = "0200"
 
-# ServiceKey = "6ZRhW%2BwEgu8KSNzXCvAsAm8fyDQ3XnPWtfbiy6rtX7JAoPeXmQMq9%2FtSczPfnLd%2FnAnPdhnNkc60BAROfx2TEQ%3D%3D"
-# with open('./locations/data_full_address.json') as json_file:
-#     json_data = json.load(json_file)
+    elif int(time) < 800 :
+        print('passed in <800')
+        time = "0500"
 
-# input_date = "2020-03-30 23:30"
+    elif int(time) < 1100 :
 
-# days = input_date.split()
-# times= days[0].split('-')
+        time = "0800"
+    
+    elif int(time) < 1400 :
 
-# api_date = times[0]+times[1]+times[2]
-# times1 = days[1].split(':')
-# api_time =times1[0]+times1[1]
-# print(api_date)
-# print(api_time)
+        time = "1100"
 
-# url = "http://apis.data.go.kr/1360000/VilageFcstInfoService/getVilageFcst?"
-# key = "serviceKey=" + ServiceKey
-# numOfRows = "&numOfRows=100"
-# type = "&dataType=JSON"
-# date = "&base_date=" + api_date
-# time = "&base_time=" + api_time
+    elif int(time) < 1700 :
 
-# location = ""
+        time = "1400"
 
-# x = (json_data['경상남도 의령군 유곡면']['x'])
-# y = (json_data['경상남도 의령군 유곡면']['y'])
+    elif int(time) < 2000 :
 
-# nx = "&nx=" + x
-# ny = "&ny=" + y
+        time = "1700"
 
-# api_url = url + key + numOfRows + type + date + time + nx + ny
-# print(date)
-# print(api_url)
-# data = urllib.request.urlopen(api_url).read().decode('utf8')
-# data_json = json.loads(data)
-# pprint(data_json)
+    elif int(time) < 2300 :
 
-# parsed_json = data_json['response']['body']['items']['item']
+        time = " 2000"
 
-# target_date = parsed_json[0]['fcstDate']  # get date and time
-# target_time = parsed_json[0]['fcstTime']
+    elif int(time) < 2400 :
 
-# date_calibrate = target_date #date of TMX, TMN
-# # if target_time > '1300':
-# #     date_calibrate = str(int(target_date) + 1)
+        time = "2300"
 
-# passing_data = {}
-# for parsed in parsed_json:
-#     passing_data[parsed['category']] = parsed['fcstValue']
+    str_day = str(day) 
+    str_month = str(month)
+    # print(str_day)
+    # print(str_month)
 
+    date = str_month+str_day
+    # print("time : " +  time)
 
-# print(passing_data)
-
-# print(passing_data['TMN'])
-# t = passing_data['T3H']
-# v = passing_data['WSD']
-# t = float(t)
-# v = float(v)
-# print(t)
-# print(v)
-# # wci = (10*math.sqrt(v)-v+10.5)*(33-t)
-# wci = 13.12 + 0.6215*t -  11.37*math.pow(v, 0.16) + 0.3965*t*math.pow(v, 0.16)
-# wci = round(wci, 2)
-# print(wci)
+    return time, date
