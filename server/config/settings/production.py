95e1a61f9c0f8d4bf848d1cb9bec319eed3084b0
Ubuntu <ubuntu@ip-172-31-42-252.ap-northeast-2.compute.internal>
2020-04-13 17:15:56 +0000

    feat: change allowed_hosts

--- server/config/settings/production.py
+++ server/config/settings/production.py
@@ -1,4 +1,4 @@
 from .base import *
 
-ALLOWED_HOSTS = ['ec2-52-79-236-214.ap-northeast-2.compute.amazonaws.com']
+ALLOWED_HOSTS = ['13.209.67.120', 'ec2-13-209-67-120.ap-northeast-2.compute.amazonaws.com']
 
