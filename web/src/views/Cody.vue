cb2ed6e8b7b7aff4e8d9fbdf6312a0192b0e50c8
SongHyunHwa <soonghh@kookmin.ac.kr>
2020-05-02 04:07:37 +0900

    fix: modify cody page

--- web/src/views/Cody.vue
+++ web/src/views/Cody.vue
@@ -1,67 +1,112 @@
 <template>
-    <div class="cody">
-        <MainNavigation></MainNavigation>
-        <div class="container">
-            <div class="add_container">
-            <router-link to="/cody/add">
-              <b-button class="btn_add" size="sm" style="margin-right:10px">등록하기</b-button>
-          </router-link>
-
-            </div>
-            <div class="row">
-                <ClassificationComponent v-bind:list="list"></ClassificationComponent>
-                <div class="col-md-10">
-                  <CodyComponent></CodyComponent>
-                </div>
-            </div>
-        </div>
-    </div>
+    <b-container>
+        <b-row algin-h="end" class="mb-3 mr-1">
+            <b-button to="/cody/add">등록하기</b-button>
+        </b-row>
+        <b-row>
+            <b-col md="2" cols="12">
+                <ClassificationComponent :list="categories" @chooseCategory="handleLowerClick"/>
+            </b-col>
+            <b-col md="10" cols="12">
+                <b-row>
+                    <b-col md="4" cols="12" class="mb-3"  v-for="clothe in clothes_set" :key="clothe.id">
+                        <ClothesSetCard :clothes_set="clothe"/>
+                    </b-col>
+                </b-row>
+            </b-col>
+        </b-row>
+    </b-container>
 </template>
 
 <script>
-import MainNavigation from '@/components/MainNavigation.vue'
 import ClassificationComponent from '@/components/ClassificationComponent.vue'
-import CodyComponent from '@/components/CodyComponent.vue'
+import ClothesSetCard from '@/components/cards/ClothesSetCard.vue'
+import axios from 'axios'
+import consts from '@/consts.js'
 export default {
-  name: 'Cody',
   components: {
-    MainNavigation,
     ClassificationComponent,
-    CodyComponent
+    ClothesSetCard
   },
   data: function () {
     return {
-      list: [
-        {
-          name: '스타일',
-          details: ['전체', '심플', '스트릿', '화려', '데이트', '정장']
-        },
-        {
-          name: '리뷰',
-          details: ['전체', '미등록']
-        }
-      ]
+      currentCategories: { lower: '', upper: '' },
+      clothes_set: []
     }
-  }
+  },
+  computed: {
+    categories: function () {
+      const CODY_CATEGORIES = consts.CODY_CATEGORIES
+      var categoryList = JSON.parse(JSON.stringify(CODY_CATEGORIES))
+      for (var i in categoryList) {
+        categoryList[i].lower.unshift('전체')
+      }
+      return categoryList
+    }
+  },
+  methods: {
+    handleLowerClick: function (upper, lower) {
+      this.currentCategories.lower = lower
+      this.currentCategories.upper = upper
+    }
+  },
+  created: function () {
+    if (!localStorage.getItem('token')) {
+      this.$router.push('/login')
+      // TODO: 에러메세지 더 좋은걸로 바꾸기.
+      alert('로그인해주세요!')
+    } else {
+      var vm = this
+      if (window.localStorage.getItem('token')) {
+        var token = window.localStorage.getItem('token')
+        var config = {
+          headers: { Authorization: `Bearer ${token}` }
+        }
+        axios.get(`${consts.SERVER_BASE_URL}/clothes-sets/`, config)
+          .then((response) => {
+            vm.clothes_set = response.data.results
+            console.log(vm.clothes)
+          }).catch((ex) => {
+          // TODO: error handling.
+          })
+      }
+    }
+  },
+  watch: {
+    currentCategories: {
+      deep: true,
+      handler () {
+        var vm = this
+        if (window.localStorage.getItem('token')) {
+          var token = window.localStorage.getItem('token')
+          var config = {
+            headers: { Authorization: `Bearer ${token}` }
+          }
+          if (vm.currentCategories.lower === '전체') {
+            axios.get(`${consts.SERVER_BASE_URL}/clothes-sets/?style`, config)
+              .then((response) => {
+                vm.clothes_set = response.data.results
+              }).catch((ex) => {
+              // TODO: error handling.
+              })
+          } else {
+            axios.get(`${consts.SERVER_BASE_URL}/clothes-sets/?style=${vm.currentCategories.lower}`, config)
+              .then((response) => {
+                vm.clothes_set = response.data.results
+              }).catch((ex) => {
+              // TODO: error handling.
+              })
+          }
+        }
+      }
 
+    }
+
+  }
 }
+
 </script>
-<style scoped>
-.cody {
-    width: 100%;
-    margin-right: auto;
-    margin-left: auto;
-    /* margin-top: 200px; */
-}
-.container_1{
-   margin-top: 100px;
-  text-align: left;
-  /* margin-left: 50px;
-  margin-right: 50px; */
-}
-.add_container{
-  width:100%;
-  text-align:right;
-  margin-bottom: 20px;
-}
+
+<style>
+
 </style>
