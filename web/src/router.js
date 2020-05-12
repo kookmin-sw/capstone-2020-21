91e5099bfd6e79168393832c877d530091b63be4
SongHyunHwa <soonghh@kookmin.ac.kr>
2020-05-12 18:18:33 +0900

    feat: cody analysis component

--- web/src/router.js
+++ web/src/router.js
@@ -11,6 +11,7 @@ import ClosetAddNew from './views/ClosetAddNew.vue'
 import Cody from './views/Cody.vue'
 import CodyAdd from './views/CodyAdd.vue'
 import CodyDetail from './views/CodyDetail.vue'
+import CodyDetailNew from './views/CodyDetailNew'
 
 Vue.use(Router)
 
@@ -72,7 +73,7 @@ export default new Router({
     {
       path: '/cody/detail',
       name: 'CodyDetail',
-      component: CodyDetail
+      component: CodyDetailNew
     }
 
   ]
