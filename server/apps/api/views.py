94bde6a1c4a22b1acfc9d935d1ccad024e6cd23d
gyqls980 <hyobin0715@kookmin.ac.kr>
2020-06-01 01:14:56 +0900

    feat: create lookbook func

--- server/apps/api/views.py
+++ server/apps/api/views.py
@@ -1,14 +1,17 @@
+from bs4 import BeautifulSoup
 import datetime
 from dateutil.parser import parse
 from django.db.models import Avg, Max, Min
 from django.utils import timezone
 from filters.mixins import FiltersMixin
 import json
+import random
 from rest_framework import viewsets, status, filters
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework_extensions.mixins import NestedViewSetMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+import requests
 from statistics import mode
 
 from .exceptions import S3FileError
@@ -250,6 +253,56 @@ class ClothesView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewSet):
             analysis_upper_category_dict[key] = mode(analysis_upper_category_dict[key])
 
         return Response(analysis_upper_category_dict)    
+
+    
+    @action(detail=False, methods=['get'])
+    def lookbook(self, request, *args, **kwargs):
+        """
+        An endpoint where the lookbook is returned
+        """
+        now = datetime.datetime.now()
+        year = str(now.year)
+        user_gender = request.user.gender
+        if user_gender=='남자':
+            user_gender = 'm'
+        else:
+            user_gender = 'f'
+
+        url = 'https://www.musinsa.com/index.php?m=shopstaff&_y=' + year + '&ordw=d_regis&gender=' + user_gender
+        
+        req = requests.get(url)
+        html = BeautifulSoup(req.text, 'html.parser')
+        div_tag = html.find('div', class_='list-box box')
+
+        # 0~9 랜덤 숫자 3개 뽑기
+        num_list = []
+        ran_num = random.randint(0, 9)
+        for count in range(3):
+            while ran_num in num_list:
+                ran_num = random.randint(0, 9)
+            num_list.append(ran_num)
+
+        li_tag = div_tag.find_all('li', class_='listItem')
+        
+        lookbook_list = []
+        for count in range(3):
+            ran_img = int(num_list[count])
+            ran_li = li_tag[ran_img]
+
+            # 이미지 url 받아오기
+            img_url = ran_li.find('img').get('src')
+            img_url = img_url.replace('//','http://')
+
+            # 브랜드명 받아오기
+            brand = ran_li.find('p', class_='brackets brand').text
+
+            # 모델 이름 받아오기
+            name = ran_li.find('span').text
+
+            lookbook = {'image' : img_url, 'brand' : brand, 'name' : name}
+            lookbook_list.append(lookbook)
+
+        return Response(lookbook_list)
 
 
 class ClothesNestedView(FiltersMixin, NestedViewSetMixin, viewsets.ModelViewSet):
